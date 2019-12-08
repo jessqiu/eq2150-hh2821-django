7a4c87df6f09f3e342bbd0ecd66249d51139e7b6
Haohui Hu <hh2821@columbia.edu>
2019-12-08 10:49:29 -0500

    I added stats, add, and detail. some problems seems in detail

--- Project/Squirrel/urls.py
+++ Project/Squirrel/urls.py
@@ -7,6 +7,8 @@ app_name = 'Squirrel'
 
 urlpatterns = [
     path('sightings', views.sightings, name='sightings'),
+    path('sightings/add', views.add, name='add'),
+    path('sightings/stats', views.stats, name='stats'),
     path('sightings/<str:squirrel_id>', views.details, name='details'),
     path('map', views.map, name='map'),
 ]
--- Project/Squirrel/views.py
+++ Project/Squirrel/views.py
@@ -27,6 +27,71 @@ def details(request, squirrel_id):
         }
         return render(request, 'Squirrel/detail.html', context)
 
+def add(request):
+    #Squirrel.objects.all().delete()
+    if request.method == 'POST':
+        form = SquirrelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('Squirrel:sightings')
+    else:
+        form = SquirrelForm()
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'Squirrel/add.html', context)
+
+def stats(request):
+    squirrels = Squirrel.objects.all()
+
+    max_Latitude = squirrels[0].Latitude
+    min_Latitude = squirrels[0].Latitude
+
+    max_Longitude = squirrels[0].Longitude
+    min_Longitude = squirrels[0].Longitude
+
+    list_Highlight_Fur_Color = []
+
+    list_Location = []
+
+    list_Hectare_Squirrel_Number = []
+
+    for item in squirrels:
+        if item.Latitude > max_Latitude:
+            max_Latitude = item.Latitude
+        if item.Latitude < min_Latitude:
+            min_Latitude = item.Latitude
+        if item.Longitude > max_Longitude:
+            max_Longitude = item.Longitude
+        if item.Longitude < min_Longitude:
+            min_Longitude = item.Longitude
+
+        if item.Highlight_Fur_Color not in list_Highlight_Fur_Color:
+            list_Highlight_Fur_Color.append(item.Highlight_Fur_Color)
+
+        if item.Location not in list_Location:
+            list_Location.append(item.Location)
+
+        list_Hectare_Squirrel_Number.append(item.Hectare_Squirrel_Number)
+
+        sighting_number=len(squirrels)
+
+    context = {
+            'sighting_number':sighting_number,
+            'max_Latitude': max_Latitude,
+            'min_Latitude': min_Latitude,
+            'max_Longitude': max_Longitude,
+            'min_Longitude': min_Longitude,
+            'list_Highlight_Fur_Color': list_Highlight_Fur_Color,
+            'list_Location': list_Location,
+            'max_Hectare_Squirrel_Number': max(list_Hectare_Squirrel_Number),
+            'min_Hectare_Squirrel_Number': min(list_Hectare_Squirrel_Number),
+            'avg_Hectare_Squirrel_Number': sum(list_Hectare_Squirrel_Number)/len(list_Hectare_Squirrel_Number),
+    }
+
+    return render(request, 'Squirrel/stats.html', context)
+
 def map(request):
     squirrels = Squirrel.objects.all()
     context = {
